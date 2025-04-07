540159390625a6028c5ba1933ef5795559a4a13c
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-04-07 20:39:37 +0330

    refactor: remove AlertComponent and integrate password generator functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,61 @@
 import { Component } from '@angular/core';
-import { AlertComponent } from './alert/alert.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [AlertComponent],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
 })
 export class AppComponent {
-  title = 'angular-standalone-components';
+  title = 'Your Password';
+  length: number = 0;
+  disableBtn = true;
+  private password: string = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  constructor() {
+    // this.password = 'New Value from Constructor';
+  }
+  onChangeLength(e: any) {
+    let value = e.target.value as string;
+    this.disableBtn = !(value.length > 0);
+    this.password = value;
+  }
+  public onButtonClick() {
+    const numbers = '12345678905';
+    const letters = 'abcdefABCDEF';
+    const symbols = '!@#$%^&*()_+';
+    let validPassword = '';
+    if (this.includeLetters) {
+      validPassword += letters;
+    }
+    if (this.includeNumbers) {
+      validPassword += numbers;
+    }
+    if (this.includeSymbols) {
+      validPassword += symbols;
+    }
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      let index = Math.floor(Math.random() * validPassword.length);
+      generatedPassword += validPassword[index];
+    }
+    this.password = generatedPassword;
+  }
+  onChangeSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  onChangeNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  getPassword(): string {
+    return this.password;
+  }
 }
