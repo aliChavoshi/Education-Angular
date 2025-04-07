bbf6a555cb3ed60f60571feaddc734b0c07fdc8f
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-04-07 19:07:19 +0330

    some changes

---
+++ src/app/alert/alert.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  imports: [],
+  templateUrl: './alert.component.html',
+  styleUrl: './alert.component.css',
+  standalone : true,
+  // encapsulation : ViewEncapsulation.Emulated,
+})
+export class AlertComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AlertComponent } from './alert/alert.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AlertComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
 })
 export class AppComponent {
-  title = 'Education';
+  title = 'angular-standalone-components';
 }
