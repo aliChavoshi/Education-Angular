f6a21032af3f306c5983bd7496c0cc9ae89f9580
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-04-09 22:16:14 +0330

    feat: implement password generator functionality in CardComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule, CardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  // template: `<app-card />`,
+  // styleUrl: './app.component.css',
   standalone: true,
 })
 export class AppComponent {
-  showMyPass = false;
-  title = 'Your Password';
-  length: number = 0;
-  disableBtn = true;
-  private password: string = '';
-  includeLetters = false;
-  includeNumbers = false;
-  includeSymbols = false;
-
-  constructor() {
-    // this.password = 'New Value from Constructor';
-  }
-  onChangeLength(e: any) {
-    let value = e.target.value as string;
-    this.disableBtn = !(value.length > 0);
-    this.password = value;
-  }
-  public onButtonClick() {
-    const numbers = '12345678905';
-    const letters = 'abcdefABCDEF';
-    const symbols = '!@#$%^&*()_+';
-    let validPassword = '';
-    if (this.includeLetters) {
-      validPassword += letters;
-    }
-    if (this.includeNumbers) {
-      validPassword += numbers;
-    }
-    if (this.includeSymbols) {
-      validPassword += symbols;
-    }
-    let generatedPassword = '';
-    for (let i = 0; i < this.length; i++) {
-      let index = Math.floor(Math.random() * validPassword.length);
-      generatedPassword += validPassword[index];
-    }
-    this.password = generatedPassword;
-    this.showMyPass = true;
-  }
-  onChangeSymbols() {
-    this.includeSymbols = !this.includeSymbols;
-  }
-  onChangeNumbers() {
-    this.includeNumbers = !this.includeNumbers;
-  }
-  onChangeLetters() {
-    this.includeLetters = !this.includeLetters;
-  }
-  getPassword(): string {
-    return this.password;
-  }
+  courses = ['C#', 'Angular', 'CSS'];
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,71 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-card',
+  imports: [FormsModule, CommonModule],
+  // template: `<p>
+  //   Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iusto atque
+  //   pariatur, exercitationem unde quidem recusandae ullam quod facere deserunt
+  //   animi laboriosam, quibusdam neque harum, voluptate sit consequatur tempora
+  //   ea.
+  // </p>`,
+  // styles : ['h1 { f}']
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
+  showMyPass = false;
+  title = 'Your Password';
+  length: number = 0;
 
-  constructor() { }
+  disableBtn = true;
+  private password: string = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
 
-  ngOnInit() {
+  constructor() {
+    // this.password = 'New Value from Constructor';
   }
-
+  onChangeLength(e: any) {
+    let value = e.target.value as string;
+    this.disableBtn = !(value.length > 0);
+    this.password = value;
+  }
+  public onButtonClick() {
+    const numbers = '12345678905';
+    const letters = 'abcdefABCDEF';
+    const symbols = '!@#$%^&*()_+';
+    let validPassword = '';
+    if (this.includeLetters) {
+      validPassword += letters;
+    }
+    if (this.includeNumbers) {
+      validPassword += numbers;
+    }
+    if (this.includeSymbols) {
+      validPassword += symbols;
+    }
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      let index = Math.floor(Math.random() * validPassword.length);
+      generatedPassword += validPassword[index];
+    }
+    this.password = generatedPassword;
+    this.showMyPass = true;
+  }
+  onChangeSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  onChangeNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  getPassword(): string {
+    return this.password;
+  }
 }
