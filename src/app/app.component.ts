454059e737b3596a2ee9628fd169b4b2fcf7dd21
Ali Chavoshi <alichavoshii1372@gmail.com>
2025-04-12 20:33:49 +0330

    feat: update app and card components to display product list with details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,47 @@ import { CardComponent } from './card/card.component';
   standalone: true,
 })
 export class AppComponent {
-  myCourses = ['C#', 'Angular', 'CSS'];
+  title = "parent title";
+  products = [
+    {
+      id: 1,
+      name: 'لپ‌تاپ ایسوس مدل X515',
+      price: 35000000,
+      imageUrl: 'assets/images/laptop.jpg',
+      isAvailable: true,
+    },
+    {
+      id: 2,
+      name: 'موس بی‌سیم لاجیتک',
+      price: 400000,
+      imageUrl: 'assets/images/mouse.jpg',
+      isAvailable: false,
+    },
+    {
+      id: 3,
+      name: 'کیبورد مکانیکی ردراگون',
+      price: 1200000,
+      imageUrl: 'assets/images/keyboard.jpg',
+      isAvailable: true,
+    },
+    {
+      id: 4,
+      name: 'مانیتور ۲۴ اینچ سامسونگ',
+      price: 7500000,
+      imageUrl: 'assets/images/monitor.jpg',
+      isAvailable: true,
+    },
+    {
+      id: 5,
+      name: 'هدست گیمینگ ایسوس',
+      price: 2200000,
+      imageUrl: 'assets/images/headset.jpg',
+      isAvailable: false,
+    },
+  ];
+
+  onAvailable(value:any){
+    // this.products = this.products.filter((product) => product.id !== value.id);
+    console.log("🚀 ~ AppComponent ~ onAvailable ~ value:", value);
+  }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,73 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+interface IProduct{
 
+}
 @Component({
   selector: 'app-card',
-  imports: [FormsModule, CommonModule],
-  // template: `<p>
-  //   Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iusto atque
-  //   pariatur, exercitationem unde quidem recusandae ullam quod facere deserunt
-  //   animi laboriosam, quibusdam neque harum, voluptate sit consequatur tempora
-  //   ea.
-  // </p>`,
-  // styles : ['h1 { f}']
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
+  standalone: true,
+  
 })
-export class CardComponent {
-  @Input({required : true}) courses!: string[];
-  //
-  showMyPass = false;
-  title = 'Your Password';
-  length: number = 0;
+export class CardComponent implements OnInit {
+  @Output() isAvailable = new EventEmitter<IProduct>(); //child => parent
 
-  disableBtn = true;
-  private password: string = '';
-  includeLetters = false;
-  includeNumbers = false;
-  includeSymbols = false;
+  private _title: string = '';
 
-  constructor() {
-    // this.password = 'New Value from Constructor';
+  @Input({ required: true }) // parent => child
+  get title(): string {
+    return this._title;
   }
-  onChangeLength(e: any) {
-    let value = e.target.value as string;
-    this.disableBtn = !(value.length > 0);
-    this.password = value;
+  set title(parentValue: number) {
+    //parent => child
+    this._title = parentValue + ' - ریال';
   }
-  public onButtonClick() {
-    const numbers = '12345678905';
-    const letters = 'abcdefABCDEF';
-    const symbols = '!@#$%^&*()_+';
-    let validPassword = '';
-    if (this.includeLetters) {
-      validPassword += letters;
-    }
-    if (this.includeNumbers) {
-      validPassword += numbers;
-    }
-    if (this.includeSymbols) {
-      validPassword += symbols;
-    }
-    let generatedPassword = '';
-    for (let i = 0; i < this.length; i++) {
-      let index = Math.floor(Math.random() * validPassword.length);
-      generatedPassword += validPassword[index];
-    }
-    this.password = generatedPassword;
-    this.showMyPass = true;
-  }
-  onChangeSymbols() {
-    this.includeSymbols = !this.includeSymbols;
-  }
-  onChangeNumbers() {
-    this.includeNumbers = !this.includeNumbers;
-  }
-  onChangeLetters() {
-    this.includeLetters = !this.includeLetters;
-  }
-  getPassword(): string {
-    return this.password;
+
+  @Input() product: any;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onClick(){
+    this.isAvailable.emit({id : 1 , name : 'my title'});
   }
 }
